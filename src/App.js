91a696d060aad696e31838043148c21cfa4575b0
TylerKerch <kerchtc@gmail.com>
2022-11-08 16:18:28 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import {Fragment, useState} from "react";
 
 function App() {
+  const[value, setValue] = useState(0);
+
+  function zero() {
+    //0
+  }
+
+  function add() {
+    //1
+  }
+
+  function subtract() {
+    //2
+  }
+
+  function multiply() {
+    //3
+  }
+
+  function divide() {
+    //4
+  }
+
+  function remainder() {
+    //5
+  }
+
+  function square() {
+    //6
+  }
+
+  function cube() {
+    //7
+  }
+
+  function root() {
+    //8
+  }
+
+  function inverse() {
+    //9
+  }
+
+  function changeSigns() {
+    //10
+  }
+
+  function random() {
+    //11
+  }
+
+  function setNumber(event) {
+    if(!isNaN(event.target.value)){
+      setValue(event.target.value);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div className="App">
+        <header className="header">
+          <input type="text" value={value} onChange={setNumber}>
+          </input>
+        </header>
+        <div className="grid-container">
+          <button onClick={zero}>Set To 0</button>
+          <button onClick={add}>Add 1</button>
+          <button onClick={subtract}>Subtract 1</button>
+          <button onClick={multiply}>Multiply By 2</button>
+          <button onClick={divide}>Divide By 2</button>
+          <button onClick={remainder}>Remainder of 2</button>
+          <button onClick={square}>Square The Number</button>
+          <button onClick={cube}>Cube The Number</button>
+          <button onClick={root}>Square Root The Number</button>
+          <button onClick={inverse}>Get The Inverse</button>
+          <button onClick={changeSigns}>Change The Sign</button>
+          <button onClick={random}>Set To Random Number 0-100</button>
+        </div>
+      </div>
+      <div className="Footer">
+        <h1>
+          Created by
+        </h1>
+      </div>
+    </div> 
   );
 }
 
